a34d4a94bfc115f139b140bd047d5f8d46fe8953
Prashant Sharma <prashanttonusharma@gmail.com>
2023-02-26 19:31:06 +0530

    26-02-23

---
+++ Merge Sort for Linked List.cpp
@@ -0,0 +1,90 @@
+//https://practice.geeksforgeeks.org/problems/sort-a-linked-list/1
+
+class Solution{
+  private:
+    Node * findmid(Node* head)
+    {
+        Node* slow = head;
+        Node * fast = head->next;
+        
+        while(fast!=NULL && fast -> next!= NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        return slow;
+    }
+    Node * merge(Node * left , Node* right)
+    {
+        if(left == NULL)
+        {
+            return right;
+        }
+        
+        if(right == NULL)
+        {
+            return left;
+        }
+        
+        Node * ans = new Node(-1);
+        Node * temp = ans;
+        
+        while(left != NULL && right != NULL)
+        {
+            if(left -> data < right -> data)
+            {
+                temp -> next = left;
+                temp = left;
+                left = left -> next; 
+            }
+            else
+            {
+                temp -> next = right;
+                temp = right;
+                right = right -> next;
+            }
+        }
+        
+        while(left!= NULL)
+        {
+            temp -> next = left;
+            temp = left;
+            left = left -> next;
+        }
+        
+        while(right!= NULL)
+        {
+            temp -> next = right;
+            temp = right;
+            right = right -> next;
+        }
+        
+        
+        ans = ans ->next;
+        
+        return ans;
+    }
+  public:
+    //Function to sort the given linked list using Merge Sort.
+    Node* mergeSort(Node* head) {
+        // your code here
+        if(head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+        
+        Node * mid = findmid(head);
+        
+        Node* left = head;
+        Node * right = mid->next;
+        mid->next = NULL;
+        
+        left = mergeSort(left);
+        right = mergeSort(right);
+        
+        Node * result = merge(left , right);
+        
+        return result;
+    }
+};
