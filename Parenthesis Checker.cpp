c1ccac872dbd50c5fe0e573ab2cf054a96a5551b
Prashant Sharma <prashanttonusharma@gmail.com>
2023-03-08 00:10:38 +0530

    08-03-23

--- Parenthesis Checker.cpp
+++ Parenthesis Checker.cpp
@@ -6,34 +6,40 @@ class Solution
     {
         // Your code here
         stack<char> s;
-        
-        for(int i = 0 ; i< x.size() ; i++)
-        {
-            char ch = x[i];
+        for(int i=0; i<x.length(); i++) {
+         
+         char ch = x[i];
+         
+         
+         if(ch == '(' || ch == '{' || ch == '['){
+             s.push(ch);
+         }
+         else
+         {
             
-            if(ch == '[' || ch == '{' || ch == '(')
-            {
-                s.push(ch);
-            }
-            
-            else
-            {
-                if((ch == '}' && s.top() == '{')  || (ch == ')' && s.top() == '(') || (ch == ']' && s.top() == '[')){
-                    s.pop();
-                }
-                else{
-                    return false;
-                }
-            }
-        }
-        
-        if(s.empty())
-        {
-            return true;
-        }
-        else{
-            return false;
-        }
+             if(!s.empty()) {
+                  char top = s.top();
+                  if( (ch == ')' && top == '(') || 
+                     ( ch == '}' && top == '{') || 
+                     (ch == ']' && top == '[') ) {
+                      s.pop();
+                  }
+                 else
+                 {
+                     return false;
+                 }
+             }
+             else
+             {
+                 return false;
+             } 
+         }  
+     }
+    
+    if(s.empty())
+        return true;
+    else
+        return false;
     }
 
 };
