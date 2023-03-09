84354c161875b57ea1ac9c1f09e9858de2d2ac70
Prashant Sharma <prashanttonusharma@gmail.com>
2023-03-09 23:27:05 +0530

    09-03-23

---
+++ Reverse a Stack.cpp
@@ -0,0 +1,31 @@
+class Solution{
+public:
+    void insertAtBottom(stack<int> &St,int x){
+        
+        if(St.empty())
+        {
+            St.push(x);
+            return;
+        }
+        
+        int num = St.top();
+        St.pop();
+        
+        insertAtBottom(St , x);
+        St.push(num);
+        
+    }
+    void Reverse(stack<int> &St){
+        
+        if(St.empty())
+        {
+            return;
+        }
+        
+        int num = St.top();
+        St.pop();
+        
+        Reverse(St);
+        insertAtBottom(St , num);
+    }
+};
