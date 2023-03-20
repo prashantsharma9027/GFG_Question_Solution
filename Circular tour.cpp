942828beb7d6ad7ad40629ed79f3e4742dab05a3
Prashant Sharma <prashanttonusharma@gmail.com>
2023-03-20 17:30:56 +0530

    20-03-23

---
+++ Circular tour.cpp
@@ -0,0 +1,30 @@
+class Solution{
+  public:
+  
+    //Function to find starting point where the truck can start to get through
+    //the complete circle without exhausting its petrol in between.
+    int tour(petrolPump p[],int n)
+    {
+       //Your code here
+       int defici = 0;
+       int balance = 0;
+       int start = 0;
+       
+       
+       for(int i = 0 ; i<n ; i++)
+       {
+           balance += p[i].petrol - p[i].distance;
+           
+           if(balance < 0)
+           {
+               defici += balance;
+               start = i+1;
+               balance = 0;
+           }
+       }
+       
+       if(defici + balance >= 0) return start;
+       
+       return -1;
+    }
+};
