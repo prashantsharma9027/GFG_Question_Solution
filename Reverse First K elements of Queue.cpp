773ce7c1e25dedf76c26d20dfcb38a9cfc3da71c
Prashant Sharma <prashanttonusharma@gmail.com>
2023-03-20 07:10:12 +0530

    23-03-23

---
+++ Reverse First K elements of Queue.cpp
@@ -0,0 +1,30 @@
+queue<int> modifyQueue(queue<int> q, int k) {
+    // add code here.
+    
+    stack<int> s;
+    
+    for(int i = 0 ; i< k ; i++)
+    {
+        int element = q.front();
+        q.pop();
+        s.push(element);
+    }
+    
+    
+    while(!s.empty())
+    {
+        int element = s.top();
+        s.pop();
+        q.push(element);
+    }
+    
+    int t = q.size() - k;
+    
+    while(t--)
+    {
+        int element = q.front();
+        q.pop();
+        q.push(element);
+    }
+    return q;
+}
