5006683eaf0ba585e388fe4030058fa3973f8726
Prashant Sharma <prashanttonusharma@gmail.com>
2023-03-25 22:33:17 +0530

    25-03-23

--- Check if two arrays are equal or not.cpp
+++ Check if two arrays are equal or not.cpp
@@ -8,7 +8,7 @@ class Solution{
         sort(A.begin(),A.end());
         sort(B.begin(),B.end());
     
-        for(int i = 0 ; i< N ; i++)
+        for(int i = 0 ;  i< N ; i++)
         {
             if(A[i]!=B[i])
                 return false; 
