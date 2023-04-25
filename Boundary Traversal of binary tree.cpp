c6be6bc3b234954365ce283dd75aca24b9e6757b
Prashant Sharma <prashanttonusharma@gmail.com>
2023-04-25 23:53:38 +0530

    25-04-23

---
+++ Boundary Traversal of binary tree.cpp
@@ -0,0 +1,64 @@
+class Solution {
+private:
+    void traverseLeft(Node *root , vector<int> &ans)
+    {
+        if((root == NULL) || (root->left == NULL && root->right == NULL)){
+            return;
+        }
+            
+        ans.push_back(root->data);
+        
+        if(root->left)
+        {
+            traverseLeft(root->left,ans);
+        }
+        else
+        {
+            traverseLeft(root->right,ans);
+        }
+    }
+    
+    void traverseLeaves(Node* root , vector<int> &ans)
+    {
+        if(root == NULL)
+            return;
+            
+        if(root->left == NULL && root->right == NULL)
+        {
+            ans.push_back(root->data);
+        }
+        
+        traverseLeaves(root->left ,ans);
+        traverseLeaves(root->right , ans);
+    }
+    
+    void traverseRight(Node * root , vector<int> &ans)
+    {
+        if((root == NULL) || (root->left == NULL && root->right == NULL))
+            return;
+            
+        if(root->right) traverseRight(root->right ,ans);
+        else traverseRight(root->left ,ans);
+        
+        ans.push_back(root->data);
+    }
+public:
+    vector <int> boundary(Node *root)
+    {
+        //Your code here
+        vector<int> ans;
+        
+        ans.push_back(root->data);
+        
+        
+        traverseLeft(root->left , ans);
+        
+        traverseLeaves(root->left , ans);
+        traverseLeaves(root->right , ans);
+        
+        traverseRight(root->right , ans);
+        
+        
+        return ans;
+    }
+};
