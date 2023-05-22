48bbadb01b505cc4355e43bf8062b09b109da431
Prashant Sharma <prashanttonusharma@gmail.com>
2023-05-22 20:01:17 +0530

    22-05-23

--- Topological sort.cpp
+++ Topological sort.cpp
@@ -1,21 +1,21 @@
 class Solution
 {
-    private:
-    void ts(vector<int> adj[] ,unordered_map<int,bool> &visited , stack<int> &st , int node)
-    {
-        visited[node] = true;
+    // private:
+    // void ts(vector<int> adj[] ,unordered_map<int,bool> &visited , stack<int> &st , int node)
+    // {
+    //     visited[node] = true;
         
         
-        for(auto neighbour : adj[node])
-        {
-            if(!visited[neighbour])
-            {
-                ts(adj , visited , st , neighbour);
-            }
-        }
+    //     for(auto neighbour : adj[node])
+    //     {
+    //         if(!visited[neighbour])
+    //         {
+    //             ts(adj , visited , st , neighbour);
+    //         }
+    //     }
         
-        st.push(node);
-    }
+    //     st.push(node);
+    // }
     
 	public:
 	//Function to return list containing vertices in Topological order. 
@@ -23,24 +23,65 @@ class Solution
 	{
 	    // code here
 	    
-	    unordered_map<int,bool> visited;
+	   // unordered_map<int,bool> visited;
 	    vector<int> ans;
-	    stack<int> st;
+	   // stack<int> st;
 	    
 	    
-	    for(int i = 0 ; i<V ; i++)
-	    {
-	        if(!visited[i])
-	            ts(adj , visited , st , i);
-	    }
+	   // for(int i = 0 ; i<V ; i++)
+	   // {
+	   //     if(!visited[i])
+	   //         ts(adj , visited , st , i);
+	   // }
 	    
-	    while(st.size()>0)
-	    {
-	        ans.push_back(st.top());
-	        st.pop();
-	    }
+	   // while(st.size()>0)
+	   // {
+	   //     ans.push_back(st.top());
+	   //     st.pop();
+	   // }
 	    
 	    
-	    return ans;
+	   // return ans;
+	   
+	   
+	   //           BFS
+	   
+	   vector<int>indegree(V,0);
+	   queue<int> q;
+	   
+	   for(int i=0;i<V;i++)
+       {
+           for(auto it : adj[i])
+           {
+               indegree[it]++;
+           }
+       }
+       
+	   
+	   for(int i = 0 ; i<V ; i++)
+	   {
+	       if(indegree[i] == 0)
+	            q.push(i);
+	   }
+	   
+	   
+	   
+	   while(!q.empty())
+	   {
+	       int front = q.front();
+	       q.pop();
+	       ans.push_back(front);
+	       
+	       for(auto neighbour : adj[front])
+	       {
+	           indegree[neighbour]--;
+	           if(indegree[neighbour] == 0)
+	                q.push(neighbour);
+	       }
+	   }
+	   
+	   return ans;
+	   
+	   
 	}
 };
